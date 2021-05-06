9e328cb5d1efe9b9248f72bd218394af9f059f1a
Benedict Weichselbaum <b.weichselbaum@mailbox.org>
2021-05-06 13:25:23 +0200

    INIT

---
+++ src/main/java/dhbw/cloudia/splitter/SalutaionNameSplitterApplication.java
@@ -0,0 +1,13 @@
+package dhbw.cloudia.splitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalutaionNameSplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalutaionNameSplitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dhbw/cloudia/splitter/SalutaionNameSplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package dhbw.cloudia.splitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalutaionNameSplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
